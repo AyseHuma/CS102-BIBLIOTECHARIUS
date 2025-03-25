9d03c9a6ae613932dc5c9be2f5d6b2c3b3cb6179
dogaotcu <doga.otcu@ug.bilkent.edu.tr>
2025-03-25 19:48:15 +0300

    Update Player.java
    added stars option to show the level of the player and updated categories so that the program can show all the points from subcategories, as well

--- Player.java
+++ Player.java
@@ -4,33 +4,63 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Player {
+    // Player sınıfının özellikleri
     private String name;
     private int ID;
     private int totalPoints;
-    private int categoryPoints;
+    private ArrayList<Integer> categoryPoints;
 
-    public Player(String name, int id, int totalPoints, int categoryPoints) 
+    public Player(String name, int id, int totalPoints, ArrayList<Integer> categoryPoints) 
     {
         this.name = name;
         this.ID = id;
         this.totalPoints = totalPoints;
         this.categoryPoints = categoryPoints;
     }
-  
+
+    public String getStarsForCategory(int categoryPoint) 
+    {
+        if (categoryPoint > 60) 
+        {
+            return "★★★";
+        } 
+        else if (categoryPoint > 45) 
+        {
+            return "★★"; 
+        } 
+        else if (categoryPoint > 30) 
+        {
+            return "★"; 
+        }
+        else 
+        {
+            return "No Stars"; 
+        }
+    }
+
     public Stage displayPlayerInfo() 
     {
         Stage stage = new Stage();
         VBox vbox = new VBox(10);
-      
-        Label nameLabel = new Label("Player Name: " + name);
-        Label idLabel = new Label("Player ID: " + ID);
-        Label totalPointsLabel = new Label("Total Points: " + totalPoints);
-        Label categoryPointsLabel = new Label("Category Points: " + categoryPoints);
 
-        vbox.getChildren().addAll(nameLabel, idLabel, totalPointsLabel, categoryPointsLabel);
+        Label nameLabel = new Label("Player Name: " + this.name);
+        Label idLabel = new Label("Player ID: " + this.ID);
+        Label totalPointsLabel = new Label("Total Points: " + this.totalPoints);
 
-        Scene scene = new Scene(vbox, 300, 200);
+        vbox.getChildren().addAll(nameLabel, idLabel, totalPointsLabel);
+
+        for (int i = 0; i < categoryPoints.size(); i++) 
+        {
+            int categoryPoint = this.categoryPoints.get(i);
+            String stars = getStarsForCategory(categoryPoint);
+            Label categoryLabel = new Label("Category " + (i + 1) + " Points: " + categoryPoint + " " + stars);
+            vbox.getChildren().add(categoryLabel);
+        }
+
+        Scene scene = new Scene(vbox, 300, 250);
         stage.setTitle("Player Information");
         stage.setScene(scene);
         stage.show();
@@ -41,7 +71,7 @@ public class Player {
     // Getter and Setter Methods
     public String getName() 
     {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) 
@@ -63,26 +93,24 @@ public class Player {
         return this.totalPoints;
     }
 
-    public void setTotalPoints(int totalPoints) {
+    public void setTotalPoints(int totalPoints) 
+    {
         this.totalPoints = totalPoints;
     }
 
-    public int getCategoryPoints() 
+    public ArrayList<Integer> getCategoryPoints() 
     {
         return this.categoryPoints;
     }
 
-    public void setCategoryPoints(int categoryPoints) 
+    public void setCategoryPoints(ArrayList<Integer> categoryPoints) 
     {
         this.categoryPoints = categoryPoints;
     }
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Application.launch(PlayerApp.class, args);
     }
 }
-
-
 
 

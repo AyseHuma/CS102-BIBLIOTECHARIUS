8d498f8af3178476b48956ab6b588a1e76ebd449
hale <hale.btl011@gmail.com>
2025-05-13 18:20:52 +0300

    Delete cleanup-ansys-labb20119-10384.bat

--- cleanup-ansys-labb20119-10384.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set LOCALHOST=%COMPUTERNAME%
-if /i "%LOCALHOST%"=="labb20119" (taskkill /f /pid 7632)
-if /i "%LOCALHOST%"=="labb20119" (taskkill /f /pid 788)
-if /i "%LOCALHOST%"=="labb20119" (taskkill /f /pid 12640)
-if /i "%LOCALHOST%"=="labb20119" (taskkill /f /pid 10384)
-
-del /F cleanup-ansys-labb20119-10384.bat
